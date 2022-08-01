29dcd5dfc8ef2d8a7e0ef9315818e6ad28cc38c0
Nisarg7021 <95787542+Nisarg7021@users.noreply.github.com>
2022-08-01 22:36:51 +0530

    Update script.py

--- script.py
+++ script.py
@@ -48,13 +48,13 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @TMMAINCHANNEL</b>
+<b>© @FULL_OF_ENTERTAINMENT</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TECHNO MINDZ Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : FOE Filter Bot</b>
 
-⭕️<b>Creater :</b> @Technomindzyt   
+⭕️<b>Creater :</b> @njbcontact_bot  
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
